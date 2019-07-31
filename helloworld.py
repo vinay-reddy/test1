b23f1716205edacc33d097d5f40afa6c51354222
vinay-reddy <vinaykbada@gmail.com>
2019-07-31 18:41:26 +0530

    commit 2

--- helloworld.py
+++
@@ -1,6 +0,0 @@
-
-import logging
-logging.basicConfig(filename='test.log',level=logging.DEBUG)
-logging.debug('this is a message. specially debug message')
-logging.info('so this is info message')
-logging.warning('this is a warning message')
