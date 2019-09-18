589c89daa7d5d4e6ae6d5dfb5c99770888ad00fe
vinay-reddy <vinaykbada@gmail.com>
2019-09-18 09:43:01 +0530

    https_statistics updated-allfiles-2.

--- updated_http_statistics.py
+++ updated_http_statistics.py
@@ -2,9 +2,19 @@
 import os
 import sys
 import pandas as pd
+import subprocess
 
+# define a function to find the start and end time
+#start_time would be first line of last file
+#end_time would be last line of first file
 
+def start_and_end_time(file_list):
+    end_time = subprocess.check_output(['tail', '-1', file_list[0]]).decode('utf8').split(',')[1]
+    start_time = subprocess.check_output(['head', '-1', file_list[-1]]).decode('utf8').split(',')[1]
+    return end_time, start_time
 
+# finding the list of files present in the http folder.
+# no. of http, ssl files and corresponding csv files as lists.
 def http_folder_files(path):
     http_ssl_files = []
     http_files = []
@@ -25,6 +35,11 @@ def http_folder_files(path):
 
 http_files, http_ssl_files, http_ssl_csv, http_csv = http_folder_files('/Users/vinayreddy/Desktop/logs/gregory-server-performance/')
 http_ssl_csv.sort()
+
+'''
+generating / skipping the generation of csv files for each ssl and http_access files.
+
+'''
 
 if len(http_ssl_csv) == 0 and len(http_csv) == 0:
     for i in range(len(http_ssl_files)):
@@ -47,19 +62,18 @@ if len(http_ssl_csv) == 0 and len(http_csv) == 0:
                 fh2.write(a[0] + ',' + a[3].split('[')[1]+','+ a[5].split('"')[1] + ',' + a[-1].split('µ')[0] +',' + a[6]+','+ a[7].split('"')[0] + '\n')
         fh2.close()
 
-
-pd.options.display.max_rows = 1200
+pd.options.display.max_rows = 120000
 pd.options.display.max_colwidth = 1000
+
 columns = ['IP address','Date and Time','HTTP Methods', 'Time taken', 'URL', 'HTTP']
-
-
 
 http_files, http_ssl_files, http_ssl_csv, http_csv = http_folder_files('/Users/vinayreddy/Desktop/logs/gregory-server-performance/')
 http_ssl_csv.sort()
 
-#==========
+# Generation of dataframe names
 df = ['dframe'+str(i) for i in range(len(http_ssl_csv))]
 list_of_dfs = {}
+# Zipping the df and ssl_csv files
 for dfra, file in zip(df, http_ssl_csv):
     list_of_dfs[dfra] = pd.read_csv(file, names = columns, header=None)
 
@@ -83,17 +97,20 @@ for i in range(len(df_http)):
 
 concated = pd.concat(list_to_cat, ignore_index=True)
 
-end_time = concated['Date and Time'][0]
-start_time = concated.iloc[-1,:][1]
+date = concated.sort_values(by=['Date and Time'], ascending=False)
 
-
+# SSL -- concated files sorted by highest time taken (b)
 b = concated.sort_values(by=['Time taken'], ascending=False)
 print(b)
 
+# SSL -- URL to count (c_count)
 series = b['URL']
 c = series.value_counts(sort=True).to_frame()
-print(c[:30])
+c.reset_index(inplace=True)
+c_count = c.rename(columns={'index': 'URL', 'URL':'Count'})
+print(c_count[:30])
 
+# SSL -- IP to count (ip_series_count)
 ip_series = b['IP address'].value_counts(sort=True).to_frame()
 
 ip_series.reset_index(inplace=True)
@@ -103,30 +120,28 @@ print(ip_series_count)
 #============ need to add this ========
 
 
-concated_http = pd.concat(list_to_cat_http, ignore_index=True)
+concated_http = pd.concat(list_to_cat_http, ignore_index=True )
 
-end_time_http = concated_http['Date and Time'][0]
-start_time_http = concated.iloc[-1,:][1]
-
-
+# HTTP -- concated files sorted by highest time taken (b_http)
 b_http = concated_http.sort_values(by=['Time taken'], ascending=False)
+
 print(b_http)
 
+# HTTP -- URL to count (c_http_count)
 series_http = b_http['URL']
 
-c_http = series.value_counts(sort=True).to_frame()
+c_http = series_http.value_counts(sort=True).to_frame()
 c_http.reset_index(inplace=True)
 c_http_count = c_http.rename(columns={'index': 'URL', 'URL':'Count'})
 print(c_http_count[:30])
 
+# HTTP -- IP to count (ip_series_count_http)
 
+ip_series_http = b_http['IP address'].value_counts(sort=True).to_frame()
 
-ip_series = b_http['IP address'].value_counts(sort=True).to_frame()
-
-ip_series.reset_index(inplace=True)
-ip_series_count = ip_series.rename(columns={'index': 'IP address', 'IP address': 'Count'})
-print('===============')
-print(ip_series_count)
+ip_series_http.reset_index(inplace=True)
+ip_series_count_http = ip_series_http.rename(columns={'index': 'IP address', 'IP address': 'Count'})
+print(ip_series_count_http)
 
 
 # function with parameter as IP address and returns all the http requests done by that IP.
@@ -138,6 +153,10 @@ print(ip_series_count)
 # req_per_ip =http_requests_per_ip('10.147.243.37')
 # print(req_per_ip)
 
+
+ssl_end_time, ssl_start_time = start_and_end_time(http_ssl_csv)
+
+http_end_time, http_start_time = start_and_end_time(http_csv)
 
 '''
     To-Do in this:
@@ -163,7 +182,7 @@ Issues fixed:
 2.  Fixed csv files size increasing issue every time we run the script. Now, csv files gets created first time you run the script and will not increase every time you run the script.
 
 '''
-print('end time http:', end_time_http)
-print('start time http:', start_time_http)
-print('end time SSL:', end_time)
-print('start time SSL:', start_time)
+print('end time http:', http_end_time)
+print('start time http:', http_start_time)
+print('end time SSL:', ssl_end_time)
+print('start time SSL:', ssl_start_time)
