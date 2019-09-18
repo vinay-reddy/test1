619af75ebcd68d7b32423f807c6f2cb3054b6cb5
vinay-reddy <vinaykbada@gmail.com>
2019-09-18 12:59:30 +0530

    https_statistics updated-allfiles-2.

--- load_avg_cpu_io_usage.py
+++ load_avg_cpu_io_usage.py
@@ -14,16 +14,14 @@ def system_load_monitor_folder_files(path):
     for f, sf, files in os.walk(path):
         if '/PolicyManagerLogs/system-load-monitor' in f:
             for file in files:
-                if 'csv' in file and 'system' in file:
-                    system_load_files_csv.append(f+'/'+file)
-                elif 'system-load' in file and 'csv' not in file:
+                if 'system-load' in file:
                     system_load_files.append(f+'/'+file)
-            return system_load_files, system_load_files_csv
+            return system_load_files
 
-system_load_files, system_load_files_csv = system_load_monitor_folder_files('/Users/vinayreddy/Desktop/logs/gregory-server-performance/')
+system_load_files = system_load_monitor_folder_files('/Users/vinayreddy/Desktop/logs/gregory-server-performance/')
 
 print(system_load_files)
-print(system_load_files_csv)
+
 
 time=[]
 one_min_load_avg = []
@@ -32,20 +30,19 @@ fifteen_min_load_avg = []
 cpu_us = []
 cpu_sy = []
 io = []
-if len(system_load_files_csv) == 0 :
-    for i in range(len(system_load_files)):
 
-        with open(system_load_files[i], 'r') as fh2:
-            a = fh2.read()
-            b = regex_object.findall(a)
-            for i in range(len(b)):
-                time.append(b[i][0])
-                one_min_load_avg.append(b[i][3].strip())
-                five_min_load_avg.append(b[i][4].strip())
-                fifteen_min_load_avg.append(b[i][5].strip())
-                cpu_us.append(b[i][7].strip())
-                cpu_sy.append(b[i][8].strip())
-                io.append(b[i][9].strip())
+for i in range(len(system_load_files)):
+    with open(system_load_files[i], 'r') as fh2:
+        a = fh2.read()
+        b = regex_object.findall(a)
+        for i in range(len(b)):
+            time.append(b[i][0].strip())
+            one_min_load_avg.append(b[i][3].strip())
+            five_min_load_avg.append(b[i][4].strip())
+            fifteen_min_load_avg.append(b[i][5].strip())
+            cpu_us.append(b[i][7].strip())
+            cpu_sy.append(b[i][8].strip())
+            io.append(b[i][9].strip())
 
             # for i in range(len(b)):
             #
@@ -59,3 +56,11 @@ print(cpu_sy)
 print(cpu_us)
 print(io)
 
+
+print(len(time))
+print(len(one_min_load_avg))
+print(len(five_min_load_avg))
+print(len(fifteen_min_load_avg))
+print(len(cpu_sy))
+print(len(cpu_us))
+print(len(io))
